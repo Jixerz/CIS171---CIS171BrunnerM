2c6379dfedb0219864a09c0b099fff6e4af97f2a
Renee Riley <rileyre@otc.edu>
2018-03-08 11:49:54 -0600

    Update Exercise15_32.java

--- Exercise15_32/src/exercise15_32/Exercise15_32.java
+++ Exercise15_32/src/exercise15_32/Exercise15_32.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
+/// Very clean code
 /**
  *
  * @author Jixer
